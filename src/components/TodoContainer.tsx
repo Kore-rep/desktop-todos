c22e2fbbbdcc69ed3c11252a99f59bc7926d4bec
Angus Longmore <anguslongmora@gmail.com>
2024-04-30 09:11:12 +0200

    Intial commit

---
+++ src/components/TodoContainer.tsx
@@ -0,0 +1,9 @@
+import '../renderer/App.css';
+
+export default function TodosContainer() {
+  return (
+    <div className="w-90 rounded-md bg-gray-500 dark:bg-slate-800">
+      Some content
+    </div>
+  );
+}
